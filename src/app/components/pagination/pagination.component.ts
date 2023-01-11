4443db449025d2f40fcb61f00b5d34f763f03fa1
biaoli <biaoli6@hotmail.com>
2023-01-11 17:42:28 -0500

    2 files modified

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -1,13 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.css']
 })
 export class PaginationComponent implements OnInit {
-  picsum : any;
+  picsum : Observable<any>;
   constructor( private httpClient : HttpClient ) { }
 
   ngOnInit() : void {
--- src/app/services/picsum.service.ts
+++ src/app/services/picsum.service.ts
@@ -1,12 +1,16 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
-@Injectable()
+@Injectable({
+  providedIn : 'root',
+})
+
 export class PicsumService {
 
   constructor(private httpClient : HttpClient ) { }
 
-  getPicsum( pageNo : number ) : any {
+  getPicsum( pageNo : number ) : Observable<any> {
     return this.httpClient.get('https://picsum.photos/v2/list?page=' + pageNo + '&limit=8')
   }
 }
